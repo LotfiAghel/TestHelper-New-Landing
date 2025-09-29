3b750e225f0aea4908857267f374825b08d28765
Thehaku <hakooochiha@gmail.com>
2025-09-29 15:30:53 +0330

    SEO update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
 import { HomeScreen } from "./home-screen";
 
+export const metadata = {
+    title: "تست‌هلپر | آمادگی آزمون‌های آیلتس، تافل، GRE و PTE",
+    description:
+        "آمادگی کامل برای آزمون‌های آیلتس (IELTS)، تافل (TOEFL)، جی‌آرای (GRE) و پی‌تی‌ای (PTE) با تست‌هلپر: نمونه سؤال، ماک رایگان، تحلیل عملکرد و منابع آموزشی.",
+    keywords: [
+        "آیلتس",
+        "IELTS",
+        "تافل",
+        "TOEFL",
+        "GRE",
+        "جی‌آرای",
+        "PTE",
+        "پی‌تی‌ای",
+        "آزمون زبان",
+        "آزمون آزمایشی",
+        "ماک آزمون",
+    ],
+};
+
 export default HomeScreen;
