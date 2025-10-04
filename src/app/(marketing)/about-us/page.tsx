83f251b05191c150be0c8cfe85dbb3117783eb51
iTestHelepr <itesthelper@gmail.com>
2025-10-04 04:05:28 +0330

    feat: create About Us page component with header, team, and footer sections

---
+++ src/app/(marketing)/about-us/page.tsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import { About } from "@/components/marketing/about/about";
+import { FooterMain } from "@/components/marketing/footers/footer-main";
+import { Header } from "@/components/marketing/header-navigation/header";
+import { Team } from "@/components/marketing/team/team";
+
+export const AboutUsPage = () => {
+    return (
+        <Fragment>
+            <Header />
+            <About />
+            <Team />
+            <FooterMain />
+        </Fragment>
+    );
+};
+
+export default AboutUsPage;
---
+++ src/components/marketing/about/about.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { type Article, Simple03Horizontal, Simple03Vertical } from "@/components/marketing/blog/base-components/blog-cards";
+import { useBreakpoint } from "@/hooks/use-breakpoint";
+import { cx } from "@/utils/cx";
+
+export const About = () => {
+    return (
+        <section className="py-8 shadow-xs sm:py-12 lg:py-16">
+            <div className="mx-auto max-w-container px-4 md:px-8">
+                <div className="mx-auto flex w-full flex-col items-center text-center">
+                    {/* <span className="text-sm font-semibold text-brand-secondary md:text-md">We're hiring!</span> */}
+                    <h2 className="mt-3 text-display-sm font-semibold text-primary md:text-display-md">درباره ما</h2>
+                    {/* <p className="mt-4 text-lg text-tertiary md:mt-5 md:text-xl">درباره ما </p> */}
+                </div>
+                <div className="mx-auto mt-8 max-w-3xl text-justify sm:mt-10 md:mt-12 lg:mt-16">
+                    <p className="mt-4 text-lg text-tertiary md:mt-5 md:text-xl">
+                        از همون روز اول که تصمیم گرفتیم تست‌هلپر رو راه بندازیم، یه مأموریت مشخص داشتیم: کمک به داوطلبانِ آزمون‌های زبان. اسم پلتفرم هم دقیقاً
+                        با همین ایده انتخاب شد.
+                    </p>
+                    <p className="mt-4 text-lg text-tertiary md:mt-5 md:text-xl">
+                        با تجربه چندین ساله در آموزش زبان انگلیسی، اولین قدممون ساخت یه پلتفرم شبیه‌ساز آزمون تافل بود؛ جایی که می‌تونی با نمونه سؤالات واقعی
+                        تمرین کنی و از امکانات آموزشی متنوع استفاده کنی. بعدش آزمون آیلتس و جی‌آرای هم اضافه شد و حالا داریم آماده‌سازی بقیه آزمون‌ها رو هم
+                        توسعه می‌دیم.
+                    </p>
+                    <p className="mt-4 text-lg text-tertiary md:mt-5 md:text-xl">
+                        راستی، تو تیم تست‌هلپر همیشه دنبال آدمای خلاق، باانرژی و پرانگیزه‌ایم، مثل خیلی از اعضای فعلی که یه روز کاربر بودن و حالا بخشی از تیم
+                        شدن. اگه دوست داری همراه ما رشد کنی، خوشحال می‌شیم تو هم به جمعمون بپیوندی!
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
