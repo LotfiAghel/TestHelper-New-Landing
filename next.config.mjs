8ae0ac4de3b9c890b98ce0583f3eed0ad7a5d8d0
iTestHelepr <itesthelper@gmail.com>
2025-11-28 18:41:20 +0330

    refactor: add image qualities in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,14 @@ const nextConfig = {
     compress: true,
     poweredByHeader: false,
     images: {
-        formats: ['image/avif', 'image/webp'],
+        formats: ["image/avif", "image/webp"],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 60,
+        qualities: [75, 85, 100],
     },
     compiler: {
-        removeConsole: process.env.NODE_ENV === 'production',
+        removeConsole: process.env.NODE_ENV === "production",
     },
 };
 
